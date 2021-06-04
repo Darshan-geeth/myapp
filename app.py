338d36fdd8bb1d1567b17214e3273165c1e3b85b
Darshan-geeth <83585475+Darshan-geeth@users.noreply.github.com>
2021-06-04 09:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     #return render_template('home.html')
-    return "Flask inside Docker!! and pushed into docker hub repo"
+    return "Flask inside Docker!! and pushed into docker hub repo !! Hello everyone"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(debug=True,host='0.0.0.0',port=port)
